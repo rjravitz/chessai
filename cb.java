035bffa515a3528d9ef854bfef1b302de3f93e24
Ryan Ravitz <rjravitz@gmail.com>
2020-08-04 12:47:29 -0400

    added basic pawn promotion

--- cb.java
+++ cb.java
@@ -16,6 +16,13 @@ public class cb {
 	static int kdxs[] = {0, 1, 1, 1, 0, -1, -1, -1}; //to check if opposite king puts the king in check
 	static int kdys[] = {1, 1, 0, -1, -1, -1, 0, 1};
 	
+	
+	public static int pfreq[] = {1, 1, 2, 2, 2, 8};
+	public static Piece[] bpieces = {new King(true), new Queen(true), new Rook(true), new Bishop(true), new Knight(true), new Pawn(true)};
+	public static Piece[] wpieces = {new King(false), new Queen(false), new Rook(false), new Bishop(false), new Knight(false), new Pawn(false)};
+	
+	
+ 	
 	public cb() {
 		board = new Piece[8][8]; tb = new Piece[8][8];
 		for(int i = 0; i < 8; i++) {
@@ -54,6 +61,23 @@ public class cb {
         
 	}
 	
+	static void pawnPromotion(Piece[][] b, boolean team, int x, int y) {
+		int freq[] = {0, 0, 0, 0, 0, 0};
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				if(b[i][j] != null && b[i][j].team == team) freq[b[i][j].rank]++;
+			}
+		}
+		freq[5]--;
+		for(int i = 0; i <= 5; i++) {
+			if(freq[i] < pfreq[i]) {
+				if(team) b[x][y] = bpieces[i];
+				else b[x][y] = wpieces[i];
+				break;
+			}
+		}
+	}
+	
     static void drawSquare(int i, int j) {
     	if ((i + j) % 2 != 0) StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
         else  StdDraw.setPenColor(StdDraw.DARK_GRAY);
@@ -63,6 +87,8 @@ public class cb {
     static void execMove(Piece[][] b, int sx, int sy, int ex, int ey) {
     	b[ex][ey] = b[sx][sy];
 		b[sx][sy] = null;
+		if(ey == 0 && b[ex][ey].team && b[ex][ey].rank == 5) pawnPromotion(b, b[ex][ey].team, ex, ey);
+		else if(ey == 7 && !b[ex][ey].team && b[ex][ey].rank == 5) pawnPromotion(b, b[ex][ey].team, ex, ey);
 		if(b[ex][ey] != null && b[ex][ey].team && b[ex][ey].rank == 0) {tbkx = ex; tbky = ey; twkx = wkx; twky = wky;}
 		else if(b[ex][ey] != null && !b[ex][ey].team && b[ex][ey].rank == 0) {tbkx = bkx; tbky = bky; twkx = ex; twky = ey;}
 		else {tbkx = bkx; tbky = bky; twkx = wkx; twky = wky;}
@@ -99,9 +125,8 @@ public class cb {
             				if(board[i][j] != null && board[i][j].team) {
             					ArrayList<Move> a = board[i][j].getValidMoves(i, j);
             					if(!a.isEmpty()) {
-            						board[a.get(0).x][a.get(0).y] = board[i][j];
-            	        			board[i][j] = null;
-            	        			if(board[a.get(0).x][a.get(0).y].rank == 0) {bkx = a.get(0).x; bky = a.get(0).y;}
+            						execMove(board, i, j, a.get(0).x, a.get(0).y);
+            						if(board[a.get(0).x][a.get(0).y].rank == 0) {wkx = a.get(0).x; wky = a.get(0).y;}
             	        			done = true;
             	        			b.draw(); StdDraw.pause(20); StdDraw.show(); 
             	        			break;
